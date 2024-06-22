4f0d0a5958432904499bd1c5f83fe4e3376d3bd2
Robert Wiesel <127550128+Bjogert@users.noreply.github.com>
2024-06-22 21:35:45 +0000

    Update Vite configuration and package.json for GitHub Pages deployment
    - Updated `vite.config.js`:
      - Set `base` to `/FireBanReplit/` for correct GitHub Pages paths
      - Specified output directory as `dist`
      - Included `index.html` in Rollup options
    
    - Updated `package.json`:
      - Set `homepage` to "https://bjogert.github.io/FireBanReplit/"
      - Verified scripts for development, build, preview, and deployment
    
    - Adjusted paths in `index.html`:
      - Ensured correct paths for CSS and JS imports

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,15 @@ import react from '@vitejs/plugin-react';
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 export default defineConfig({
-  base: './',
+  base: '/FireBanReplit/',
   plugins: [
     react(),
     cssInjectedByJsPlugin(),
   ],
   build: {
+    outDir: 'dist',
     rollupOptions: {
-      input: './src/index.jsx'
+      input: './index.html'
     }
   },
   server: {
